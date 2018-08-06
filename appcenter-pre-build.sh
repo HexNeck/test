5079c5c12c3906e7fd412ebf5707b061ddbd5658
Bartosz Slowi <bartosz.slowi@gmail.com>
2018-08-06 12:29:07 +0200

    brewing esLint

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,4 +2,6 @@
 
 # Example: Change bundle name of an iOS app for non-production
 echo "lint it's checking itself? "
+brew install eslint
+brew link --overwrite eslint
 node_modules/.bin/eslint --ignore-path .gitignore .
